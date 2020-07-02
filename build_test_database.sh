b02bc9da299613b2f2e89fb2c935017ad888d05f
Dana Gretton <dana.gretton@gmail.com>
2020-07-02 11:36:22 -0400

    auto-run fragment picking when building test database

--- build_test_database.sh
+++ build_test_database.sh
@@ -1,1 +1,3 @@
-python seq_file_to_frag_set.py resources/genbank/e_coli/e_coli_genbank.faa resources/genbank/e_coli_aa.fragset.json && python build_db_from_frags.py resources/hazard_db/hazards.fragset.json
+python funtrp_sequence_pick.py --noplot &&
+python seq_file_to_frag_set.py resources/genbank/e_coli/e_coli_genbank.faa resources/genbank/e_coli_aa.fragset.json &&
+python build_db_from_frags.py resources/hazard_db/hazards.fragset.json
