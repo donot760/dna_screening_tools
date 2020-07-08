1ae47b0092c5e4dbd3ae6c3388ec7e9ae5c97827
Camila <cmirandallovera@gmail.com>
2020-07-08 00:00:59 -0400

    provides best sequences for csvs in funtrp_results

--- funtrp_sequence_pick.py
+++ funtrp_sequence_pick.py
@@ -6,46 +6,62 @@ Created on Mon Jun 29 12:44:50 2020
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 import csv
-import sys
-plotting = "--noplot" not in sys.argv
-if plotting:   
-    import matplotlib.pyplot as plt
+import os
 
 def list_files(directory):
-    pass
+#    os.chdir(directory)
+    current_dir = os.getcwd()
+
+    new_dir = os.chdir(os.getcwd() + "/../funtrp_results/")#goes up a directory to access funtrp results 
+    csv_files = os.listdir(new_dir)#gets all the csv files
+
+
+    return os.getcwd(), csv_files[2:]#first two items are not csv files
 
 def residue_and_toggles(filename):
     untouched_toggles = []
     residues = []
+    rheos = []
+    neutrals = []
     
     with open(filename) as csv_file:
         csv_reader  = csv.reader(csv_file, delimiter = ',')
         next(csv_reader)#skip the header
         for line in csv_reader:#this goes through every line
             prediction_t = float(line[5])#prediction toggle
+            prediction_r = float(line[9])
+            prediction_n = float(line[6])
             residue = line[11]
             untouched_toggles.append(prediction_t)#these prediction toggles are straight from the data
             residues.append(residue)
-#    print(residues, untouched_toggles)
-    return residues, untouched_toggles
+            rheos.append(prediction_r)
+            neutrals.append(prediction_n)
+
+    return residues, untouched_toggles, rheos, neutrals
 
 def shift_toggles(toggle_values, shift):
     
-    shifted_toggles = list(map(lambda toggle: toggle + 0.3, toggle_values))
+    shifted_toggles = list(map(lambda toggle: toggle + shift, toggle_values))
 #    print(shifted_toggles)
     return shifted_toggles
 
-def create_possible_sequences(shifted_toggles, residues, window = 19):
+def create_possible_sequences(residues, toggles, rheos, neutrals, window = 19):
     count = 0
+    shifted_toggles = shift_toggles(toggles, .3)#changed to have the shift here, enter origianl toggle values instead
     possible_tval_sequences = []
     possible_residue_sequences = []
+    t_r_n = []#toggles, neutrals, and rheos
     for toggle in shifted_toggles:#error: going to have the same number in the data, will think that something with an index of 130 is actually a value with index of 2
         if (count + window) <= len(shifted_toggles):
             possible_tval_sequences.append(shifted_toggles[count: count + window])
             possible_residue_sequences.append(residues[count: count + window])
-        count += 1
+            t_r_n.append(toggles[count: count + window] + rheos[count: count + window] + neutrals[count: count + window])
             
+            
+        count += 1
+    
     for index in range(len(possible_tval_sequences)):
         possible_tval_sequences[index] = np.prod(possible_tval_sequences[index])#get the product for each sequence
         
@@ -75,17 +91,17 @@ def create_possible_sequences(shifted_toggles, residues, window = 19):
         if possible_residue_sequences.index(residue_sequence) in index_values:
             filtered_sequence.append(residue_sequence)
 #            possible_residue_sequences.remove(residue_sequence)
-    print(filtered_sequence)
+#    print(filtered_sequence)
     
     string_list = []
-    with open("resources/aa_fragment_picks.txt","w+") as f:
+    with open("aa_fragment_picks.txt","w+") as f:
         for sequence in filtered_sequence:
             f.write("{}\r\n".format(sequence))
     
     
-    with open("resources/aa_fragment_picks.txt","r") as f:
+    with open("aa_fragment_picks.txt","r") as f:
         lines = f.readlines()
-        print(lines)
+#        print(lines)
     
     
     for line in lines:
@@ -96,64 +112,42 @@ def create_possible_sequences(shifted_toggles, residues, window = 19):
         string_list.append(new_string)
 #    print(string_list)
 
-    with open("resources/aa_fragment_picks.txt","w+") as f:
+    with open("aa_fragment_picks.txt","w+") as f:
         for string in string_list:
             f.write("{}\r\n".format(string))
    
         
     return possible_tval_sequences, possible_residue_sequences
 
-def products_for_sequences(list_of_sequences, residues):
-    
-    for index in range(len(list_of_sequences)):
-        list_of_sequences[index] = np.prod(list_of_sequences[index])#get the product for each sequence
-        
-#    string_list = []
-#    with open("resources/aa_fragment_picks.txt","w+") as f:
-#        for sequence in list_of_sequences:
-#            f.write("{}\r\n".format(sequence))
+#def products_for_sequences(list_of_sequences, residues):
 #    
-#    
-#    with open("resources/aa_fragment_picks.txt","r") as f:
-#        lines = f.readlines()
-#        print(lines)
-#    
-#    
-#    for line in lines:
-#        new_string = ""
-#        for aa in line:
-#            if aa.isalpha():
-#                new_string += aa
-#        string_list.append(new_string)
-##    print(string_list)
-#
-#    with open("resources/aa_fragment_picks.txt","w+") as f:
-#        for string in string_list:
-#            f.write("{}\r\n".format(string))
-    
-    return list_of_sequences
+#    for index in range(len(list_of_sequences)):
+#        list_of_sequences[index] = np.prod(list_of_sequences[index])#get the product for each sequence
+#        
+#    return list_of_sequences
 
-if plotting:
-    def graph(sequence_products):
-        plt.plot(range(len(sequence_products)), sequence_products)
-        plt.show()
-    
-#def filter_(threshold, final_products):
-#    copied = final_products.copy()
-#    count = 0
-#    while count < len(copied):
-#        if final_products[count] < threshold:
-#            final_products.remove(final_products[count])
-#            count += 1
+#def graph(sequence_products):
 #    
-#    print(final_products)
-            
+#    plt.plot(range(len(sequence_products)), sequence_products)
+#    plt.show()
+    
+
     
 if __name__ == '__main__':
-    r_t = residue_and_toggles("resources/select_agent_hazard/funtrp_results/NP_042046_funtrp.csv")
-    shifted_values = (shift_toggles(r_t[1], .3))
-    tval_possibilities = create_possible_sequences(shifted_values, r_t[0])
-    products = products_for_sequences(tval_possibilities[0], tval_possibilities[1])
+    directory = list_files("UROP 2020")
+#    files = os.listdir(directory)[1:]
+#    new_dir = os.chdir(os.getcwd() + "/../funtrp_results/")
+#    os.getcwd()
+#    list_files(new_dir)
+    files = directory[1]
+    for file in files:
 
-    if plotting:
-        graph(products)
+#        print("current csv: ", end = '')
+#        print(file)
+        r_t = residue_and_toggles(directory[0] +"/"+ str(file))
+#    r_t = residue_and_toggles(os.getcwd() + "/../funtrp_results/NP_042046_funtrp.csv")
+#    shifted_values = (shift_toggles(r_t[1], .3))
+        tval_possibilities = create_possible_sequences(r_t[0], r_t[1] ,r_t[2], r_t[3])
+#    products = products_for_sequences(tval_possibilities[0], tval_possibilities[1])
+    
+#    graph(products)
