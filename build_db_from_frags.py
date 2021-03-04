48f76b0bccab85f8900d7ce4b2e98f379d4485ed
Dana Gretton <dana.gretton@gmail.com>
2021-03-04 20:24:13 +0000

    changes on ec2 instance, memory reporting etc

--- build_db_from_frags.py
+++ build_db_from_frags.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import time
 from Bio.Seq import Seq
 from ecoli_seq_variants import single_variants
 from metro_hastings_variants import MetroHastingsVariants
@@ -13,6 +14,8 @@ if running_as_script and len(sys.argv) < 2:
 
 output_path = sys.argv[-1]
 known_seq_path = os.path.join('resources', 'known_seqs')
+
+start_time = time.time()
 
 # Locally, the known-sequence set fits in memory. On the cloud, it's potentially terabytes, and is built and run totally differently.
 running_on_cloud = '--cloud' in sys.argv
@@ -35,6 +38,11 @@ else:
 if running_as_script:
     hazard_set = []
     print('generating and filtering variants...')
+    if os.path.isfile('resources/hazard_scinames.txt'):
+        with open('resources/hazard_scinames.txt') as f:
+            hazard_scinames = [line.strip().lower() for line in f]
+    else:
+        hazard_scinames = []
     with open('resources/aa_fragment_picks.txt') as f:
         for funtrp_line in f:
             funtrp_line = funtrp_line.strip()
@@ -43,7 +51,7 @@ if running_as_script:
             aa_frag, ntr_triples = parse_funtrp_line(funtrp_line)
             single_variant_set = set((str(Seq(v).translate()) for v in single_variants(aa_frag)))
             #print('num single variants:', len(single_variant_set))
-            variant_computer = MetroHastingsVariants(aa_frag, ntr_triples, 10000)
+            variant_computer = MetroHastingsVariants(aa_frag, ntr_triples, 1000000)
             all_variants = single_variant_set | variant_computer.result_set()
             #print('num Metro-Hastings variants:', len(variant_computer.result_set()))
             for variant in all_variants: # includes original (zero-variant) too
@@ -52,6 +60,9 @@ if running_as_script:
                     print('not including', variant)
                     continue
                 hazard_set.append(variant)
+    print(time.time() - start_time)
+    tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
+    print(used_m/tot_m*100, '% memory used')
     print('done')
 
     if running_on_cloud:
@@ -61,30 +72,64 @@ if running_as_script:
                 if i + window <= len(str_in):
                     yield str_in[i:i+window]
 
-        approx_batch_size = 8e6 # a few dozen megabytes at a time
+        approx_batch_size = 8 # a few dozen megabytes at a time
         hazard_set = frozenset(hazard_set)
-        with open('.' + output_path + '_temp', 'w+') as f:
-            f.write(json.dumps(hazard_set, indent=2)) #TODO: do we want to be generating this temp file every time?
+        with open(os.path.join(os.path.dirname(output_path), '.' + os.path.basename(output_path) + '_temp'), 'w+') as f:
+            f.write(json.dumps(list(hazard_set), indent=2)) #TODO: do we want to be generating this temp file every time?
 
         def known_seqs_in_hazard_set(known_seq_fname):
-            from Bio import SeqIO
-            window_batch = set()
+            #from Bio import SeqIO
+            #window_batch = set()
             intersect_set = set()
-            for record in SeqIO.parse(known_seq_fname, "fasta"): # potentially many GBs
-                if len(window_batch) < approx_batch_size:
-                    window_batch.update(split_into_windows(str(record.seq)))
-                    continue
-                intersect_set.update(set.intersection(hazard_set, window_batch))
-                window_batch = set()
-            intersect_set.update(set.intersection(hazard_set, window_batch))
+            seq = ''
+            #for record in SeqIO.parse(known_seq_fname, "fasta"): # potentially many GBs
+            skipping = False
+            with open(known_seq_fname) as f: # potentially many GBs
+                for line in f:
+                    if not line:
+                        continue
+                    if line[0] == '>':
+                        #window_batch.update(split_into_windows(seq))
+                        for window in split_into_windows(seq):
+                            if window in hazard_set:
+                                intersect_set.add(window)
+                        #if len(window_batch) > approx_batch_size:
+                        #    intersect_set.update(window_batch & hazard_set)
+                        #    window_batch = set()
+                        #    if intersect_set:
+                        #        print(known_seq_fname + ':', intersect_set)
+                        line = line.lower()
+                        skipping = any(sciname in line for sciname in hazard_scinames) # go to skip mode to skip over the following sequence
+                        seq = ''
+                        continue
+                    if skipping:
+                        continue
+                    seq += line.strip()
+            # don't forget leftover/partially complete items
+            #window_batch.update(split_into_windows(seq))
+            #intersect_set.update(window_batch & hazard_set)
+            for window in split_into_windows(seq):
+                if window in hazard_set:
+                    intersect_set.add(window)
+            if intersect_set:
+                print(known_seq_fname + ':', intersect_set)
             return intersect_set
 
+        indiv_paths = [os.path.join(known_seq_path, fname) for fname in os.listdir(known_seq_path) if fname[0] != '.']
+        indiv_paths = [p for p in indiv_paths if os.path.isfile(p)]
+        print(indiv_paths)
+
+        print('initial size of hazard set:', len(hazard_set))
         with multiprocessing.Pool(processes=cpus) as pool:
-            known_hazard_seqs = set.union(pool.imap_unordered(known_seqs_in_hazard_set, os.listdir(known_seq_path)))
+            known_hazard_seqs = set.union(*pool.imap_unordered(known_seqs_in_hazard_set, indiv_paths))
+        print('removing', len(known_hazard_seqs), 'known hazard sequence fragment(s)')
+        if len(known_hazard_seqs) < 100:
+            print(known_hazard_seqs)
         hazard_set = hazard_set - known_hazard_seqs
+        print('final size of hazard set:', len(hazard_set))
 
     print('writing output...')
     with open(output_path, 'w+') as f:
-        f.write(json.dumps(hazard_set, indent=2))
+        f.write(json.dumps(list(hazard_set), indent=2))
     print('done')
 
